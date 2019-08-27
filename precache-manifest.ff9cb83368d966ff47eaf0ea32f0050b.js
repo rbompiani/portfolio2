1b427e9f6e253ac2c7330f755ee567d5fca64fd6
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-08-26 21:09:10 -0700

    Updates

---
+++ precache-manifest.ff9cb83368d966ff47eaf0ea32f0050b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865a5b98c972dabd17840d8f351fecae",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "a39c963a2905e446c5fa",
+    "url": "/portfolio2/static/css/main.451650a0.chunk.css"
+  },
+  {
+    "revision": "cc95cf52ff9118fd5293",
+    "url": "/portfolio2/static/js/2.51678b07.chunk.js"
+  },
+  {
+    "revision": "a39c963a2905e446c5fa",
+    "url": "/portfolio2/static/js/main.c26f2423.chunk.js"
+  },
+  {
+    "revision": "63ccce5776ec601fbe6f",
+    "url": "/portfolio2/static/js/runtime~main.f6c2c117.js"
+  }
+]);
