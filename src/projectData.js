a6f028172c69c5b7a3e585b2d707fffe8e57156f
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-10-03 20:56:52 -0700

    added project descriptions

--- src/projectData.js
+++ src/projectData.js
@@ -4,7 +4,7 @@ const ProjectData = {
             "id": 1,
             "title": "Julia's Child",
             "short_desc": "Search, save, and collect recipes. Just like Grandma used to do, but online.",
-            "long_desc": "This is a much longer description of the project, lessons learned, and future steps",
+            "long_desc": "Version 1.0 of this online recipe box allows users to search the Edamame API to find new and exciting recipes to add to their digital recipe collection, or add their own. Version 2.0 will include a weekly meal planner, and the ability to create a grocery list from each week's meal plan",
             "year": 2019,
             "img": ["recipe1.JPG","recipe2.jpg","recipe3.jpg"],
             "github": "https://github.com/rbompiani/Project2",
@@ -14,52 +14,40 @@ const ProjectData = {
         },
         {
             "id": 2,
-            "title": "Project 2",
-            "short_desc": "This is a short description of how the project functions",
-            "long_desc": "This is a much longer description of the project, lessons learned, and future steps",
-            "year": 205,
-            "img": ["flower.jpg"],
-            "github": "www.github.com",
+            "title": "Cupboard Companion",
+            "short_desc": "Monitor and manage your inventory with sensors that log data in real time",
+            "long_desc": "With the help of Arduino and the Johnny5 Library, Users can track inventory by weight in real time. Each inventory item has an email reminder to reorder when the stock reaches a designated threshold.  Reorder inventory by clicking on the gauge's associated reorder link",
+            "year": 2019,
+            "img": ["cupboard1.JPG", "cupboard2.JPG", "cupboard3.jpg"],
+            "github": "https://github.com/rbompiani/CupboardCompanion",
             "demo": "www.rbompiani.com",
             "major_skill": "react",
             "skills": ["react","node","mysql"]
         },        
         {
             "id": 3,
-            "title": "Project 3",
-            "short_desc": "what's the big deal",
-            "long_desc": "This is a much longer description of the project, lessons learned, and future steps",
-            "year": 2019,
-            "img" : ["rabbit.jpg"],
-            "github": "www.github.com",
-            "demo": "www.rbompiani.com",
-            "major_skill": "node",
-            "skills": ["node","javascript"]
+            "title": "Dynamic 3D",
+            "short_desc": "Shop for car accessories dynamically in 3d, from the comfort of your own home",
+            "long_desc": "Levarage webGL and javascript to build dynamic 3d models. Accessories turn on and off in real time, and the camera automatically adjusts to view those accessories. A running sidebar of options and pricing keeps you up to date as you add and remove parts ",
+            "year": 2014,
+            "img" : ["auto1.jpg","auto2.jpg"],
+            "github": "https://github.com/rbompiani/",
+            "demo": "http://rbompiani.com/projects/chrysler/index.html",
+            "major_skill": "javascript",
+            "skills": ["javascript","threed"]
         },
         {
             "id": 4,
-            "title": "Project 2",
-            "short_desc": "This is a short description of how the project functions",
-            "long_desc": "This is a much longer description of the project, lessons learned, and future steps",
-            "year": 2015,
-            "img": ["flower.jpg"],
+            "title": "Interactive Campus",
+            "short_desc": "An interactive map-based interface that would allow planners, registrars, and department members alike to view their campus data more succinctly",
+            "long_desc": "In 2011 Oregon State University was struggling to provide facilities for an explosive increase in student enrollment. By aggregating existing data into interactive and visual formats, we were able to provide recommendations for efficient expansion of the campus.",
+            "year": 2011,
+            "img": ["campus1.jpg","campus2.jpg","campus3.jpg"],
             "github": "www.github.com",
-            "demo": "www.rbompiani.com",
-            "major_skill": "react",
-            "skills": ["react","node","mysql"]
-        },   
-        {
-            "id": 5,
-            "title": "Project 2",
-            "short_desc": "This is a short description of how the project functions",
-            "long_desc": "This is a much longer description of the project, lessons learned, and future steps",
-            "year": 2007,
-            "img": ["flower.jpg"],
-            "github": "www.github.com",
-            "demo": "www.rbompiani.com",
-            "major_skill": "react",
-            "skills": ["react","node","mysql"]
-        }  
+            "demo": "http://rbompiani.com/projects/testing.html",
+            "major_skill": "javascript",
+            "skills": ["javascript","mysql"]
+        } 
     ]
 
 }
