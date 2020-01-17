618c88c84786aee45768bbc9dfcd82d3f4e7dd94
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-01-16 21:11:22 -0800

    built production folder

---
+++ build-old/precache-manifest.3e285a777dc7ece6a7cb77d417b78f75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bc7eddeb49c1af3718de69cc9a441c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b19c124e0840c653272",
+    "url": "/static/css/main.7ce8f8d0.chunk.css"
+  },
+  {
+    "revision": "ffac0e0d29c4428193ea",
+    "url": "/static/js/2.f9bdc512.chunk.js"
+  },
+  {
+    "revision": "8b19c124e0840c653272",
+    "url": "/static/js/main.425d9ba3.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  }
+]);
